0fcfb7acee5d82a3d7b9775153d24c671ccdd672
w196 <w196@github.com>
2020-06-16 23:24:18 -0400

    added options to rvd

--- rvd.sh
+++ rvd.sh
@@ -1,5 +1,34 @@
+#!/bin/bash
+
 # Downloads video from a reddit post.
 # Method from u/budzen. https://www.reddit.com/r/Enhancement/comments/6yl9tt/any_way_to_get_the_direct_link_to_reddithosted/duwxtij/
+
+#Help.
+function show_help () {
+	echo "Usage:
+		-h		Show this help page.
+		-o FILE		Write output to FILE.
+	"
+	}
+
+#Options fluff.
+output_file="rvd-output"
+
+while getopts "ho:" o; do
+	case "${o}" in
+	h)	show_help & exit 0
+		;;
+	o)	output_file=$OPTARG
+		;;
+	*)	show_help & exit 0
+		;;
+	esac
+done
+shift $((OPTIND -1))
+
+if [ -z "${s}" ] || [ -z "${p}" ]; then
+	show_help & exit 0
+fi
 
 # Get json from link and extract the video and audio urls.
 media_link=$1
@@ -19,7 +48,8 @@ echo "Pulling video from $video_url:"
 	wget -O rvd-video $video_url
 
 #Combine the two with ffmpeg.
-ffmpeg -i rvd-video -i rvd-audio -c:v copy -c:a aac rvd-output.mp4
+ffmpeg -i rvd-video -i rvd-audio -c:v copy -c:a aac $output_file.mp4
 
 #Cleanup.
 rm rvd-video rvd-audio
+exit 0
